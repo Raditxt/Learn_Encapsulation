bde166a289c0334ad9913a62c875dfcebb3d22d1
Raditya Mulya Akbar <radityamulyaakbar2@gmail.com>
2024-09-25 08:51:22 +0800

    first

---
+++ src/pbo_a/Circle.java
@@ -0,0 +1,29 @@
+package pbo_a;
+
+public class Circle {
+    private double radius;
+
+    // Constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // Method to calculate the area of the circle
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    // Method to calculate the circumference of the circle
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    // Setter and getter for radius
+    protected void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
